1cb47e92b564c0fd10b146568b02aa2cbdaf5699
psong1 <paulsong29@gmail.com>
2023-06-05 00:07:05 -0400

    server is live and can be tested in apollo sandbox

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongod://localhost/tech-freeze", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/tech-freeze"
+);
 
-module.exports = mongoose.connections;
+module.exports = mongoose.connection;
