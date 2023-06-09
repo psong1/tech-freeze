451ea233bf7d829a132ac9f292da749bff6a2928
psong1 <paulsong29@gmail.com>
2023-06-08 21:21:55 -0400

    readded utils

--- notes/src/Components/LoginSignupContainer/LoginSignupContainer.jsx
+++ notes/src/Components/LoginSignupContainer/LoginSignupContainer.jsx
@@ -1,35 +1,141 @@
-import React, { useRef } from 'react';
-import { useState } from 'react';
-import Signup from '../Signup/Signup';
-import Login from '../Login/Login';
+import React, { useRef } from "react";
+import { useState, useEffect } from "react";
+import Signup from "../Signup/Signup";
+import Login from "../Login/Login";
 import "./LoginSignupContainer.css";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../../utils/mutations";
+import { Form, Button, Alert } from "react-bootstrap";
+
+import Auth from "../../utils/auth";
 
 const LoginSignupContainer = () => {
+  const [userFormData, setUserFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+  // set state for form validation
+  const [validated] = useState(false);
+  // set state for alert
+  const [showAlert, setShowAlert] = useState(false);
 
-    // defining state to make sure if login or signup is clicked
-    const [login, setLogin] = useState(true);
+  const [addUser, { error }] = useMutation(ADD_USER);
 
-    const loginSignupContainerRef = useRef(null);
-
-    const handleClick = () => {
-        setLogin(!login);
-
-        
-        loginSignupContainerRef.current.classList.toggle("active");
-
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
     }
-    
-    return (
-        <div className='login-signup-container' ref={loginSignupContainerRef}>
-       <Login/>
-       <div className='side-div'>
-        <button type='button' onClick={handleClick}> 
-        {" "}
-        {login ? "Signup" : "Login"} </button>
-</div> 
-       <Signup/>
-    </div>
-    );
+  }, [error]);
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    // check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    try {
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
+      console.log(data);
+      Auth.login(data.addUser.token);
+    } catch (err) {
+      console.error(err);
+    }
+
+    setUserFormData({
+      username: "",
+      email: "",
+      password: "",
+    });
+  };
+
+  return (
+    <>
+      {/* This is needed for the validation functionality above */}
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        {/* show alert if server response is bad */}
+        <Alert
+          dismissible
+          onClose={() => setShowAlert(false)}
+          show={showAlert}
+          variant="danger"
+        >
+          Something went wrong with your signup!
+        </Alert>
+
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="username">Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Your username"
+            name="username"
+            onChange={handleInputChange}
+            value={userFormData.username}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Username is required!
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="email">Email</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Your email address"
+            name="email"
+            onChange={handleInputChange}
+            value={userFormData.email}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Email is required!
+          </Form.Control.Feedback>
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label htmlFor="password">Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Your password"
+            name="password"
+            onChange={handleInputChange}
+            value={userFormData.password}
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Password is required!
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Button
+          disabled={
+            !(
+              userFormData.username &&
+              userFormData.email &&
+              userFormData.password
+            )
+          }
+          type="submit"
+          variant="success"
+        >
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
 };
 
 export default LoginSignupContainer;
